c3328081133f2e5e545973b545e2ea18e1ae9f18
ZenkaiDevMx <zenkaidev.mx@gmail.com>
2026-03-26 17:23:52 -0600

    feat: Python-V35-Packages-Subpackages-Logic
    Implementation of Python Packages and Subpackages using __init__.py. Organized directory structure for modular arithmetic operations and reusable code.

---
+++ uso_modulos.py
@@ -0,0 +1,29 @@
+#EJEMPLO 1
+
+# from calculos.calculos_generales import potencia
+
+# potencia(4,6)
+
+#EJEMPLO 2
+
+# from calculos.calculos_generales import *
+
+# redondear(4.8)
+
+#NOTA: Importante dejar en la raiz de tu carpeta el archivo uso_modulos.py
+#y dejar solo en tu paquete calculos el __init__.py y el calculos_generales.py
+
+#EJEMPLO 3
+
+# from calculos.basicos.operaciones_basicas import *
+
+# sumar(5,7)
+
+#NOTA: Tienes que acordarte de la ruta, tiene paquete principal (calculos), subpaquete(basicos), 
+#modulo dentro del subpaquete (operaciones_basicas) y funciones/clases que quieras importar (import, *).
+
+#EJEMPLO 4
+
+# from calculos.redondeo_potencia.redondeaYpotencia import *
+
+# potencia(5,7)
